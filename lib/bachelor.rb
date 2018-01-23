3066ca06b3470a0a17171afc1172fbbadd263970
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-01-23 14:11:20 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,16 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  data.each do |season_num, people_array|
+    if season_num == season
+      people_array.each do |stats|
+        if stats[:status] == "Winner"
+          return stats[:name].split[0]
+        end
+      end
+    end
+  end
 end
+
 
 def get_contestant_name(data, occupation)
   # code here
